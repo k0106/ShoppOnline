5a613c46b169c32f31ae6f45fdf8db2fa7d22937
Melissa <105521312+k0106@users.noreply.github.com>
2022-10-30 01:44:18 -0500

    cambios

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express =require('express')
+
+const app =express()
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>Tienda Online Ecommerce</h1>')
+})
+
+app.listen(4000,() =>{
+    console.log('Servidor escuchando en puerto 4000')
+})    
