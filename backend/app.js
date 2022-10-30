cc6af127a1cb2b56e65ed223adf35276f5ace6dd
Melissa <105521312+k0106@users.noreply.github.com>
2022-10-30 12:14:25 -0500

    cambiando variable PORT

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,17 @@
+const express =require('express')
+const express =require('express')
 require('dotenv').config()
 
-const express =require('express')
-
-const app =express()
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+const app= express
 
 app.get('/', (req, res) => {
+    console.log('Peticion recibida')
+
     res.status(200).send('<h1>Tienda Online Ecommerce LoreleyN</h1>')
+
 })
 
-const PORT=process.env.PORT||4000
+const PORT= process.env.PORT|| 4000
 
 app.listen(PORT,() =>{
     console.log('Servidor escuchando en puerto ${PORT}')
